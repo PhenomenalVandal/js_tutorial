9bc19c84bfe47d91452c6b94f2c8f0d667e715cf
Padlo <phenomenalVandalLover@gmail.com>
2022-12-27 13:57:08 +0200

    first time use git

---
+++ script.js
@@ -0,0 +1,23 @@
+/*global console*/
+// eslint-disable-line no-console
+"use strict";
+
+//  let number = 4.6;
+
+//  const obj = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false,
+//  };
+
+ //console.log(obj.name);
+//  console.log(obj["name"]);
+
+//  let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+const answers = [];
+answers[0] = prompt('Name', '');
+answers[1] = prompt('Last Name', '');
+answers[2] = prompt('How old are you?', '');
+
+console.log(typeof(answers));
